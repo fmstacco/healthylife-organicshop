4ae2a9cedf1322c7cf2ee4fe04fc2e205073df5b
fmstacco <fmstacco@gmail.com>
2023-05-24 11:00:54 +0000

    Corrected typo in settings.py 'USE_AWS'

--- organicshop/settings.py
+++ organicshop/settings.py
@@ -182,7 +182,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if '    S' in os.environ:
+if 'USE_AWS' in os.environ:
 
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
