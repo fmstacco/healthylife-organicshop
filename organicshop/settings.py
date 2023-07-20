7583ee031ad07d96814d6ea6991770a80ad1e655
fmstacco <fmstacco@gmail.com>
2023-07-20 13:19:24 +0000

    Set debug to False

--- organicshop/settings.py
+++ organicshop/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['organic-shop.herokuapp.com', 'localhost', '8000-fmstacco-healthylifeorg-w9zqseip2fh.ws-eu101.gitpod.io']
